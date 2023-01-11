ad973f2b72f7e400b7e0ec1c4f73bfeb84987ff8
Trian Damai <triannurizkillah@gmail.com>
2023-01-11 18:25:55 +0700

    feat: add create recipe

--- app/src/main/java/app/trian/resepku/feature/createRecipe/Screen.kt
+++ app/src/main/java/app/trian/resepku/feature/createRecipe/Screen.kt
@@ -1,15 +1,12 @@
 package app.trian.resepku.feature.createRecipe
 
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
